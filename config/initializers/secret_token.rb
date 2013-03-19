f6efd1ed791408a3bcbad16d8a4727f7025a2120
Jason Goodwin <justmyhead@gmail.com>
2013-03-19 13:31:36 -0400

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Alphabetstew::Application.config.secret_token = '7d46f5df132efa2fb59a3e68c0120e72f4086f3c953a7a5af173b7b7dac13f1813b4837733a0ba7125fc87ae67e7e9ee535a42d5215583f5b6467c01d1057e20'
