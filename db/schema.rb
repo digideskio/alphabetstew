0e2080c94b8fd7abc64b1c2d91550ad53d123ac4
Jason Goodwin <justmyhead@gmail.com>
2013-03-20 09:29:05 -0400

    removed letters table

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130320025522) do
+ActiveRecord::Schema.define(:version => 20130320132304) do
 
   create_table "characters", :force => true do |t|
     t.string   "description"
@@ -21,15 +21,6 @@ ActiveRecord::Schema.define(:version => 20130320025522) do
   end
 
   add_index "characters", ["user_id"], :name => "index_characters_on_user_id"
-
-  create_table "letters", :force => true do |t|
-    t.datetime "created_at",         :null => false
-    t.datetime "updated_at",         :null => false
-    t.string   "image_file_name"
-    t.string   "image_content_type"
-    t.integer  "image_file_size"
-    t.datetime "image_updated_at"
-  end
 
   create_table "users", :force => true do |t|
     t.string   "email",                  :default => "", :null => false
